6fe1552bd7d78f7e7828f2e8c914adb989b7bc05
talfour <jak.malinowski94@gmail.com>
2023-01-19 11:59:35 +0000

    Added create user functionality in admin user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,23 +10,41 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
-    ordering = ['id']
-    list_display = ['email', 'name']
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {"fields": ("email", "password")}),
         (
-            _('Permissions'),
+            _("Permissions"),
             {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
                 )
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
