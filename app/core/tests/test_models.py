e2f8bfd14045aaaee7d4301bf0a0106d49acffeb
talfour <jak.malinowski94@gmail.com>
2023-01-25 09:28:53 +0000

    added tests for Item model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,14 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(**params):
+    """Create an user"""
+    defaults = {"email": "test@example.com", "password": "testpass123"}
+    defaults.update(params)
+    user = get_user_model().objects.create_user(**defaults)
+    return user
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -14,7 +22,7 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful."""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(
+        user = create_user(
             email=email,
             password=password,
         )
@@ -32,13 +40,13 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, "sample123")
+            user = create_user(email=email, password="sample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user("", "test123")
+            create_user(email="", password="test123")
 
     def test_create_superuser(self):
         """Test creating a superuser."""
@@ -50,9 +58,16 @@ class ModelTests(TestCase):
 
     def test_create_shopping_list(self):
         """Test creating a shopping list is successful."""
-        user = get_user_model().objects.create_user("test@example.com", "testpass234")
+        user = create_user(email="test@example.com", password="testpass234")
         shopping_list = models.ShoppingList.objects.create(
             user=user, title="Sample shopping list", description="Sample description"
         )
 
         self.assertEqual(str(shopping_list), shopping_list.title)
+
+    def test_create_items(self):
+        """Test creating an item is successful."""
+        user = create_user()
+        item = models.Item.objects.create(user=user, name="Item1")
+
+        self.assertEqual(str(item), item.name)
