7c3b2d74c1b4e1367536b0e87f2341eb7cea5e89
talfour <jak.malinowski94@gmail.com>
2023-01-25 16:06:59 +0000

    adjusted serializer for creating and updating item

--- app/shopping_list/serializers.py
+++ app/shopping_list/serializers.py
@@ -18,10 +18,54 @@ class ItemSerializer(serializers.ModelSerializer):
 class ShoppingListSerializer(serializers.ModelSerializer):
     """Serializer for shopping lists."""
 
+    items = ItemSerializer(many=True, required=False)
+
     class Meta:
         model = ShoppingList
-        fields = ["id", "title", "additional_users"]
+        fields = ["id", "title", "additional_users", "items"]
         read_only_fields = ["id"]
+
+    def _get_or_create_items(self, items, shopping_list):
+        """Handle getting or creating items as needed"""
+        auth_user = self.context["request"].user
+        for item in items:
+            item_obj, create = Item.objects.get_or_create(
+                user=auth_user,
+                **item,
+            )
+            shopping_list.items.add(item_obj)
+
+    def _get_additional_user(self, additional_users, shopping_list):
+        """Handle getting an additional user."""
+        for user in additional_users:
+            shopping_list.additional_users.add(user)
+
+    def create(self, validated_data):
+        """Create a shopping list"""
+        items = validated_data.pop("items", [])
+        additional_users = validated_data.pop("additional_users", [])
+        shopping_list = ShoppingList.objects.create(**validated_data)
+        self._get_or_create_items(items, shopping_list)
+        self._get_additional_user(additional_users, shopping_list)
+
+        return shopping_list
+
+    def update(self, instance, validated_data):
+        """Update shopping list."""
+        items = validated_data.pop("items", [])
+        additional_users = validated_data.pop("additional_users", [])
+        if items is not None:
+            instance.items.clear()
+            self._get_or_create_items(items, instance)
+        if additional_users is not None:
+            instance.additional_users.clear()
+            self._get_additional_user(additional_users, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class ShoppingListDetailSerializer(ShoppingListSerializer):
