d9081cff7e25f66020638d4cf5cfca3583622cd8
talfour <jak.malinowski94@gmail.com>
2023-02-07 10:54:11 +0000

    Added React as Frontend with Vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: "3000",
+    hmr: {
+      clientPort: 3000,
+      host: "127.0.0.1",
+      protocol: "wss",
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
