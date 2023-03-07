f64530c683c7c26bcac9289d8679dcfe9b177034
talfour <jak.malinowski94@gmail.com>
2023-03-07 14:13:04 +0000

    add swagger back

--- app/app/settings.py
+++ app/app/settings.py
@@ -194,6 +194,7 @@ SIMPLE_JWT = {
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # 'rest_framework_simplejwt.authentication.JWTAuthentication', # TODO: For now
         "user.authenticate.CustomAuthentication",
