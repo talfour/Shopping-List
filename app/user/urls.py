ce9298921540718b1f915d37c7d36a8d2e723fc7
talfour <jak.malinowski94@gmail.com>
2023-03-14 09:25:25 +0000

    added url name

--- app/user/urls.py
+++ app/user/urls.py
@@ -11,7 +11,7 @@ app_name = "user"
 urlpatterns = [
     path("create", views.CreateUserView.as_view(), name="create"),
     path("me", views.ManageUserView.as_view(), name="me"),
-    path("login", views.loginView),
-    path("refresh-token", views.CookieTokenRefreshView.as_view()),
+    path("login", views.loginView, name="token-obtain"),
+    path("refresh-token", views.CookieTokenRefreshView.as_view(), name="token-refresh"),
     path("logout", views.logoutView),
 ]
