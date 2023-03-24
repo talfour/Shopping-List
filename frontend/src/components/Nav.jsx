478f801d8ae9db5932dd3efe3a64f77377b5eddd
Jakub <jak.malinowski94@gmail.com>
2023-03-24 15:29:20 +0000

    added missing navigate

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,24 +4,25 @@ import useAuth from "../hooks/useAuth";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import useLogout from "../hooks/useLogout";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const { user } = useAuth();
   const logout = useLogout();
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
 
   const handleLogout = async () => {
     setLoading(true);
     await logout();
     navigate("/auth/login");
   };
-
   return (
     <NavStyled>
       <Link to="/">
         <h1>Shopping List</h1>
       </Link>
-      {user ? (
+      {user?.name ? (
         <div>
           <Link to="auth/user">{user?.name}</Link>
           <button disabled={loading} type="button" onClick={handleLogout}>
@@ -29,7 +30,7 @@ const Nav = () => {
           </button>
         </div>
       ) : (
-        <div>
+        <div className="padding">
           <Link to="/auth/login">Login</Link>
           <Link to="/auth/register">Register</Link>
         </div>
@@ -46,7 +47,7 @@ const NavStyled = styled(motion.nav)`
   background: #37474f;
   color: white;
   box-shadow: 0px 10px 5px 0px rgba(0, 0, 0, 0.75);
-  
+
   a {
     color: #bdbdbd;
     transition: all 0.7s ease;
@@ -69,6 +70,11 @@ const NavStyled = styled(motion.nav)`
         background: none;
         color: white;
       }
+    }
+  }
+  .padding {
+    a {
+      padding: 0 1rem;
     }
   }
 `;
